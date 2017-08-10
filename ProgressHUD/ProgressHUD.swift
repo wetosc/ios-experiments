c4b6fb36bc013e50cc8766f40088b01b7d1138b5
Eugeniu Cernei <eugen.cernei@gmail.com>
2017-08-10 12:05:53 +0300

    ProgressHUD: Added some //MARK comments to the code

--- ProgressHUD/ProgressHUD.swift
+++ ProgressHUD/ProgressHUD.swift
@@ -5,56 +5,27 @@
 //  Created by Eugeniu Cernei on 5/5/17.
 //  Copyright © 2017 Eugeniu Cernei. All rights reserved.
 //
-
 import UIKit
 
+//MARK: HUDState
+
 enum HUDState {
+    
     case hidden
     case show
 }
 
+//MARK: ProgressHUD
+
 class ProgressHUD {
+    
+    //MARK: - Public Properties
+    
     public static let shared = ProgressHUD()
     
-    private init() {
-        setupBGView()
-        setupFrontView()
-        setupIndicator()
-    }
-    
-    private func setupBGView() {
-        guard let window = UIApplication.shared.keyWindow else {  return  }
-        view = UIView(frame: window.frame)
-        view?.backgroundColor = UIColor.init(white: 0.5, alpha: 0.5)
-    }
-    
-    private func setupFrontView() {
-        guard let window = UIApplication.shared.keyWindow,
-            let view = view else {  return  }
-        frontView = UIView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 100)))
-        frontView?.center = window.center
-        
-        frontView?.backgroundColor = UIColor(colorLiteralRed: 242/255,
-                                             green: 133/255,
-                                             blue: 105/255, alpha: 1)
-        frontView?.layer.cornerRadius = 15
-        view.addSubview(frontView!)
-    }
-    
-    private func setupIndicator() {
-        guard let frontView = frontView else {  return  }
-        indicator = UIActivityIndicatorView (frame: frontView.frame)
-        indicator?.center = CGPoint.init(x: frontView.frame.width/2, y: frontView.frame.width/2)
-        frontView.addSubview(indicator!)
-        indicator?.activityIndicatorViewStyle = .whiteLarge
-    }
-    
-    
-    private var view: UIView?
-    private var frontView: UIView?
-    private var indicator: UIActivityIndicatorView?
-    
     private(set) public var state: HUDState = .hidden
+    
+    //MARK: - Public Methods
     
     public func show() {
         guard state == .hidden,
@@ -70,6 +41,45 @@ class ProgressHUD {
         view?.removeFromSuperview()
         indicator?.stopAnimating()
         state = .hidden
+    }
+    
+    //MARK: - Private Properties
+    
+    private var view: UIView?
+    private var frontView: UIView?
+    private var indicator: UIActivityIndicatorView?
+    
+    //MARK: - Private Methods
+    
+    private init() {
+        setupBGView()
+        setupFrontView()
+        setupIndicator()
+    }
+    
+    private func setupBGView() {
+        guard let window = UIApplication.shared.keyWindow else {  return  }
+        view = UIView(frame: window.frame)
+        view?.backgroundColor = UIColor.clear
+    }
+    
+    private func setupFrontView() {
+        guard let window = UIApplication.shared.keyWindow,
+            let view = view else {  return  }
+        frontView = UIView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 100)))
+        frontView?.center = window.center
+        
+        frontView?.backgroundColor = UIColor(white: 0.8, alpha: 0.6)
+        frontView?.layer.cornerRadius = 5
+        view.addSubview(frontView!)
+    }
+    
+    private func setupIndicator() {
+        guard let frontView = frontView else {  return  }
+        indicator = UIActivityIndicatorView (frame: frontView.frame)
+        indicator?.center = CGPoint.init(x: frontView.frame.width/2, y: frontView.frame.width/2)
+        frontView.addSubview(indicator!)
+        indicator?.activityIndicatorViewStyle = .whiteLarge
     }
     
 }
